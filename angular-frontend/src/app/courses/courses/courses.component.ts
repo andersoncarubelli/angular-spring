08c59879c034154e1f9504a3d847631eda31f440
Anderson Carubelli <1820277+andersoncarubelli@users.noreply.github.com>
2022-07-09 18:04:44 -0300

    add courses table and loading animation
    Signed-off-by: Anderson Carubelli <1820277+andersoncarubelli@users.noreply.github.com>

--- angular-frontend/src/app/courses/courses/courses.component.ts
+++ angular-frontend/src/app/courses/courses/courses.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  courses$: Observable<Course[]>;
 
-  constructor() { }
+  displayedColumns = ['name', 'category'];
 
-  ngOnInit(): void {
+  constructor(private service: CoursesService) {
+    this.courses$ = this.service.list();
   }
 
+  ngOnInit(): void {}
 }
